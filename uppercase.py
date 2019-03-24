9de1db447dde184d17c56d1b55b50f32d7163d53
AzGhort <ghort@seznam.cz>
2019-03-25 00:00:15 +0100

    Add files via upload

--- uppercase.py
+++ uppercase.py
@@ -12,8 +12,8 @@ from uppercase_data import UppercaseData
 # Parse arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("--alphabet_size", default=0, type=int, help="If nonzero, limit alphabet to this many most frequent chars.")
-parser.add_argument("--batch_size", default=50, type=int, help="Batch size.")
-parser.add_argument("--epochs", default=10, type=int, help="Number of epochs.")
+parser.add_argument("--batch_size", default=1000, type=int, help="Batch size.")
+parser.add_argument("--epochs", default=5, type=int, help="Number of epochs.")
 parser.add_argument("--hidden_layers", default="500", type=str, help="Hidden layer configuration.")
 parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
 parser.add_argument("--window", default=2, type=int, help="Window size to use.")
@@ -41,9 +41,8 @@ model = tf.keras.Sequential([
     tf.keras.layers.Input(shape=[2 * args.window + 1], dtype=tf.int32),
     tf.keras.layers.Lambda(lambda x: tf.one_hot(x, len(uppercase_data.train.alphabet))),
     tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(100, activation=tf.nn.relu),
-    tf.keras.layers.Dense(100, activation=tf.nn.relu),
-    tf.keras.layers.Dense(100, activation=tf.nn.relu),
+    tf.keras.layers.Dense(150, activation=tf.nn.relu),
+    tf.keras.layers.Dense(150, activation=tf.nn.relu),
     tf.keras.layers.Dense(2 * args.window + 1)
 ])
 
@@ -54,9 +53,9 @@ model.compile(
 )
 
 model.fit(
-    uppercase_data.train.data, uppercase_data.train.data,
+    uppercase_data.train.data["windows"], uppercase_data.train.data["labels"],
     batch_size=args.batch_size, epochs=args.epochs,
-    validation_data=(uppercase_data.dev.data, uppercase_data.dev.data)
+    validation_data=(uppercase_data.dev.data["windows"], uppercase_data.dev.data["labels"])
 )
 
 with open("uppercase_test.txt", "w") as out_file:
