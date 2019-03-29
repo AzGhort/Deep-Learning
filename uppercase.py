efc227adb95100102ef5d81282bb6c5c2497b302
AzGhort <ghort@seznam.cz>
2019-03-29 12:26:20 +0100

    Add files via upload

--- uppercase.py
+++ uppercase.py
@@ -12,11 +12,11 @@ from uppercase_data import UppercaseData
 # Parse arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("--alphabet_size", default=0, type=int, help="If nonzero, limit alphabet to this many most frequent chars.")
-parser.add_argument("--batch_size", default=1000, type=int, help="Batch size.")
-parser.add_argument("--epochs", default=5, type=int, help="Number of epochs.")
+parser.add_argument("--batch_size", default=5000, type=int, help="Batch size.")
+parser.add_argument("--epochs", default=7, type=int, help="Number of epochs.")
 parser.add_argument("--hidden_layers", default="500", type=str, help="Hidden layer configuration.")
 parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
-parser.add_argument("--window", default=2, type=int, help="Window size to use.")
+parser.add_argument("--window", default=20, type=int, help="Window size to use.")
 args = parser.parse_args()
 args.hidden_layers = [int(hidden_layer) for hidden_layer in args.hidden_layers.split(",") if hidden_layer]
 
@@ -36,13 +36,16 @@ args.logdir = os.path.join("logs", "{}-{}-{}".format(
 # Load data
 uppercase_data = UppercaseData(args.window, args.alphabet_size)
 
-
 model = tf.keras.Sequential([
     tf.keras.layers.Input(shape=[2 * args.window + 1], dtype=tf.int32),
     tf.keras.layers.Lambda(lambda x: tf.one_hot(x, len(uppercase_data.train.alphabet))),
     tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(150, activation=tf.nn.relu),
-    tf.keras.layers.Dense(150, activation=tf.nn.relu),
+    tf.keras.layers.Dense(100, activation=tf.nn.relu),
+    tf.keras.layers.Dense(100, activation=tf.nn.relu),
+    tf.keras.layers.Dense(100, activation=tf.nn.relu),
+    tf.keras.layers.Dense(100, activation=tf.nn.relu),
+    tf.keras.layers.Dense(100, activation=tf.nn.relu),
+    tf.keras.layers.Dense(100, activation=tf.nn.relu),
     tf.keras.layers.Dense(2 * args.window + 1)
 ])
 
@@ -58,8 +61,22 @@ model.fit(
     validation_data=(uppercase_data.dev.data["windows"], uppercase_data.dev.data["labels"])
 )
 
-with open("uppercase_test.txt", "w") as out_file:
-    # TODO: Generate correctly capitalized test set.
-    # Use `uppercase_data.test.text` as input, capitalize suitable characters,
-    # and write the result to `uppercase_test.txt` file.
-    pass
+with open("uppercase_test.txt", "w", encoding="utf-8") as out_file:
+    predictions = model.predict(uppercase_data.test.data["windows"])
+    text_length = len(uppercase_data.test.text)
+    out_text = [" "] * text_length
+    i = 0
+
+    for predict in predictions:
+        gold_label = np.argmax(predict)
+        for j in range(i, min(i + 2*args.window + 1, text_length)):
+            if j == gold_label + i:
+                out_text[j] = uppercase_data.test.text[j]
+            else:
+                out_text[j] = uppercase_data.test.text[j].upper()
+        i += 1
+    print("".join(out_text), file=out_file, end="")
+
+
+# 698f4a25-47cc-11e9-b0fd-00505601122b
+# b5770ea9-40bc-11e9-b0fd-00505601122b
