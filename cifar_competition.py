b058bebdb11f8da5e2ca2c74297dc0712ee8d8f4
AzGhort <ghort@seznam.cz>
2019-04-07 09:51:52 +0200

    Update cifar_competition.py

--- cifar_competition.py
+++ cifar_competition.py
@@ -66,19 +66,21 @@ class WideNet(tf.keras.Model):
             callbacks=[self.tb_callback],
         )
 
+
 # https://arxiv.org/pdf/1512.03385.pdf
 class ResNet(tf.keras.Model):
 
-    def __init__(self, args):
+    def __init__(self, layers):
+        group_block_count = int((layers - 2)/6)
         inputs = tf.keras.layers.Input(shape=[CIFAR10.H, CIFAR10.W, CIFAR10.C])
         # 32x32x16
         conv1 = tf.keras.layers.Conv2D(filters=16,kernel_size=3,strides=1,padding='same',activation=tf.nn.relu)(inputs)
         # 32x32x32
-        conv2 = self.add_convolution_group(5, 16, False, [3, 3], 'same', conv1)
+        conv2 = self.add_convolution_group(group_block_count, 16, False, [3, 3], 'same', conv1)
         # 16x16x64
-        conv3 = self.add_convolution_group(5, 32, True, [3, 3], 'same', conv2)
+        conv3 = self.add_convolution_group(group_block_count, 32, True, [3, 3], 'same', conv2)
         # 8x8x128
-        conv4 = self.add_convolution_group(5, 64, True, [3, 3], 'same', conv3)
+        conv4 = self.add_convolution_group(group_block_count, 64, True, [3, 3], 'same', conv3)
 
         avg_pool = tf.keras.layers.GlobalAveragePooling2D()(conv4)
         outputs = tf.keras.layers.Dense(CIFAR10.LABELS, activation=tf.nn.softmax)(avg_pool)
@@ -102,25 +104,25 @@ class ResNet(tf.keras.Model):
             layer = self.add_residual_block(res_block_kernels, filters, padd, layer)
         return layer
 
-    def add_bn_relu_conv(self, filt, kernel, padd, previous):
-        first = tf.keras.layers.BatchNormalization(axis=3)(previous)
-        second = tf.keras.layers.Activation(activation=tf.nn.relu)(first)
-        layer = tf.keras.layers.Conv2D(filters=filt, kernel_size=kernel, strides=1,
-                                       padding=padd, use_bias=False, activation=None)(second)
+    def add_conv_bn_relu(self, filt, kernel, padd, previous):
+        first = tf.keras.layers.Conv2D(filters=filt, kernel_size=kernel, strides=1,
+                                       padding=padd, use_bias=False, activation=None)(previous)
+        second = tf.keras.layers.BatchNormalization(axis=3)(first)
+        layer = tf.keras.layers.Activation(activation=tf.nn.relu,)(second)
         return layer
 
     def add_residual_block(self, kernels, filt, padd, previous):
         layer = previous
         for kernel in kernels:
-            layer = self.add_bn_relu_conv(filt, kernel, padd, layer)
+            layer = self.add_conv_bn_relu(filt, kernel, padd, layer)
         hidden = tf.keras.layers.Add()([layer, previous])
         return hidden
 
-    def train(self, cifar, args):
+    def train(self, tr_i, tr_o, dev_i, dev_o, args):
         self.fit(
-            cifar.train.data["images"], cifar.train.data["labels"],
+            tr_i, tr_o,
             batch_size=args.batch_size, epochs=args.epochs,
-            validation_data=(cifar.dev.data["images"], cifar.dev.data["labels"]),
+            validation_data=(dev_i, dev_o),
             callbacks=[self.tb_callback],
         )
 
@@ -131,10 +133,11 @@ class DataAugment:
     def cutout_image_dataset(dataset, image_size, cutout_size, versions):
         images = dataset["images"]
         labels = dataset["labels"]
-        counter = 0
-        a = np.array([[1, 2, 3]])
+        new_imgs = []
+        new_lbls = []
         mean = np.mean(images)
         for image in images:
+            counter = 0
             for _ in range(versions):
                 x = int(np.random.rand()*image_size)
                 y = int(np.random.rand()*image_size)
@@ -144,11 +147,14 @@ class DataAugment:
                 new_im = np.copy(image)
                 new_label = np.copy(labels[counter])
                 new_im[x:end_x, y:end_y] = cutout
-                new_im.shape = (1, image_size, image_size, 3)
-                new_label.shape = (1, 1)
-                images = np.append(images, new_im, axis=0)
-                labels = np.append(labels, new_label, axis=0)
-                counter += 1
+                new_imgs.append(new_im)
+                new_lbls.append(new_label)
+            counter += 1
+        new_imgs = np.array(new_imgs)
+        new_lbls = np.array(new_lbls)
+        input = np.append(images, new_imgs, axis=0)
+        output = np.append(labels, new_lbls, axis=0)
+        return input, output
 
 
 if __name__ == "__main__":
@@ -159,8 +165,9 @@ if __name__ == "__main__":
 
     # Parse arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("--batch_size", default=32, type=int, help="Batch size.")
-    parser.add_argument("--epochs", default=20, type=int, help="Number of epochs.")
+    parser.add_argument("--batch_size", default=10, type=int, help="Batch size.")
+    parser.add_argument("--epochs", default=30, type=int, help="Number of epochs.")
+    parser.add_argument("--layers", default=54, type=int, help="Number of layers.")
     parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
     args = parser.parse_args()
 
@@ -171,8 +178,9 @@ if __name__ == "__main__":
     tf.config.threading.set_intra_op_parallelism_threads(args.threads)
 
     # Create logdir name
-    args.logdir = os.path.join("logs", "{}-{}-{}".format(
+    args.logdir = os.path.join("logs", "{}-{}-{}-{}".format(
         os.path.basename(__file__),
+        "ResNet" + str(args.layers),
         datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S"),
         ",".join(("{}={}".format(re.sub("(.)[^_]*_?", r"\1", key), value) for key, value in sorted(vars(args).items())))
     ))
@@ -180,13 +188,14 @@ if __name__ == "__main__":
     # Load data
     cifar = CIFAR10()
 
-    #DataAugment.cutout_image_dataset(cifar.train.data, 32, 16, 1)
-    #DataAugment.cutout_image_dataset(cifar.test.data, 32, 16, 1)
-    #DataAugment.cutout_image_dataset(cifar.dev.data, 32, 16, 1)
+    # train_in, train_out = DataAugment.cutout_image_dataset(cifar.train.data, 32, 16, 3)
+    # test_in, test_out = DataAugment.cutout_image_dataset(cifar.test.data, 32, 16, 3)
+    # dev_in, dev_out = DataAugment.cutout_image_dataset(cifar.dev.data, 32, 16, 3)
 
     # Create the network and train
-    network = ResNet(args)
-    network.train(cifar, args)
+    network = ResNet(args.layers)
+    network.train(cifar.train.data["images"], cifar.train.data["labels"],
+                  cifar.dev.data["images"], cifar.dev.data["labels"], args)
 
     # Generate test set annotations, but in args.logdir to allow parallel execution.
     with open(os.path.join(args.logdir, "cifar_competition_test.txt"), "w", encoding="utf-8") as out_file:
